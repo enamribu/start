412a9d9771149fe90c4901ad1c245b82fb70f53f
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2025-03-08 13:37:32 +0700

    Update 10 11 PRO.bat

--- 10 11 PRO.bat
+++ 10 11 PRO.bat
@@ -1,8 +1,5 @@
 @echo off
-cscript //nologo c:\windows\system32\slmgr.vbs /upk
-cscript //nologo c:\windows\system32\slmgr.vbs /cpky
 cscript //nologo c:\windows\system32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul
 cscript //nologo c:\windows\system32\slmgr.vbs /skms kms8.msguides.com >nul
 cscript //nologo c:\windows\system32\slmgr.vbs /ato
-
 del "%~f0"
